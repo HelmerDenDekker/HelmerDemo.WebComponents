cd06cc5899af573e5594e2be29fe74928647908c
Helmer Den Dekker <Helmer.DenDekker@samenlevingszaken.onmicrosoft.com>
2023-08-31 14:17:54 +0200

    addedoneslot

--- userCard.js
+++ userCard.js
@@ -1,12 +1,41 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
+    .user-card {
+        font-family: 'Arial', sans-serif;
+        background: #f4f4f4;
+        width: 500px;
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        grid-gap: 10px;
+        margin-bottom: 15px;
+        border-bottom: darkorchid 5px solid;
+    }
+    .user-card img {
+        width: 100%;
+    }
+    .user-card button {
+        cursor: pointer;
+        background: darkorchid;
+        color: #fff;
+        border: 0;
+        border-radius: 5px;
+        padding: 5px 10px;
+    }
     h3 {
-        color: coral;
+        color: #000;
     }
 </style>
 <div class="user-card">
-    <h3></h3>
+    <img />    
+    <div>    
+        <h3></h3>
+        <div class="info">
+            <p> <slot /> </p>
+            <p> Phone: </p>
+        </div>
+        <button id="toggle-info">Hide info</button>
+    </div>
 </div>
 `; 
 
@@ -19,6 +48,7 @@ class UserCard extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true)); // use the shadow root, and refer to the template.
         
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
     }
 }
 
