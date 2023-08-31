cad886c844520e42c39e364a20497d028e2cbc2b
Helmer Den Dekker <Helmer.DenDekker@samenlevingszaken.onmicrosoft.com>
2023-08-31 12:58:31 +0200

    AddedShadowrootScopedStyling

--- userCard.js
+++ userCard.js
@@ -1,7 +1,24 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+    h3 {
+        color: coral;
+    }
+</style>
+<div class="user-card">
+    <h3></h3>
+</div>
+`; 
+
 class UserCard extends HTMLElement {
     constructor() {
         super(); // call the HTMLElement class we are extending
-        this.innerHTML = 'User Name'; 
+
+        this.attachShadow({mode: 'open'}); // Add shadow DOM
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true)); // use the shadow root, and refer to the template.
+        
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
     }
 }
 
